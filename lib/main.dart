d4bfe94d6bc2df13331d8053167f20762f5a954b
Henry Chen <henrychen385@gmail.com>
2020-09-19 21:40:47 +0800

    added typography

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _AsyncTestState extends State<AsyncTest> {
   double _x, _y, _z = 0;
 
   void getData() async {
-    Timer.periodic(Duration(seconds: 3), (timer) {
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
       setState(() {
         _x = x;
         _y = y;
@@ -59,12 +59,38 @@ class _AsyncTestState extends State<AsyncTest> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Text(_x != null ? "x: " + _x.toStringAsFixed(3) : 'nothing...'),
-          Text(_y != null ? "y: " + _y.toStringAsFixed(3) : 'nothing...'),
-          Text(_z != null ? "z: " + _z.toStringAsFixed(3) : 'nothing...'),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          children: [
+            Text(
+              "Accelerometer Data",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+            Row(
+              children: [
+                Center(
+                  child: Text(
+                    _x != null ? "x: " + _x.toStringAsFixed(3) : 'nothing...',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    _y != null ? "y: " + _y.toStringAsFixed(3) : 'nothing...',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    _z != null ? "z: " + _z.toStringAsFixed(3) : 'nothing...',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -80,7 +106,7 @@ class GPSDataState extends State<GPSData> {
   Position posStream;
 
   void delayed() {
-    Timer.periodic(Duration(seconds: 3), (timer) {
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
       currPos = posStream;
     });
   }
@@ -101,6 +127,10 @@ class GPSDataState extends State<GPSData> {
     return Container(
       child: Column(
         children: [
+          Text(
+            "GPS Data",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+          ),
           Text("latitude: " +
               (currPos != null ? currPos.latitude.toString() : 'nothing...')),
           Text("longitude: " +
