1fc5ac10f25ab900836995c21f56c1340f5e0e5b
jumper385 <henrychen385@gmail.com>
2020-09-21 14:01:20 +0800

    changed color

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.amber[50],
         body: Column(
           children: [
             AsyncTest(),
