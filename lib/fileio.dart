d005d1d39b78333d4daf6a17dfcd4b80b4e6fbbe
jumper385 <henrychen385@gmail.com>
2020-11-19 15:12:49 +0800

    more fileio

--- lib/fileio.dart
+++ lib/fileio.dart
@@ -6,14 +6,20 @@ import 'package:path_provider/path_provider.dart';
 final uuid = Uuid();
 
 Future<String> writeFile(data_array, data_type, filename, vehicleId) async {
-    final Directory directory = await getApplicationDocumentsDirectory();
+  final Directory directory = await getApplicationDocumentsDirectory();
 
-    var new_file = '${directory.path}/${filename}';
-    final File file = await File(new_file).create(recursive: true);
-    final data = await file.readAsString();
-    final sink = file.openWrite();
-    sink.write(data + "${vehicleId}, ${uuid.v4()}, ${DateTime.now()}, ${data_type}, ${data_array.join(",")}\n");
-    sink.close();
+  var new_file = '${directory.path}/${filename}.txt';
+  final File file = await File(new_file).create(recursive: true);
+  final data = await file.readAsString();
+  final sink = file.openWrite();
+  sink.write(data + "${vehicleId}, ${uuid.v4()}, ${DateTime.now()}, ${data_type}, ${data_array.join(",")}\n\r");
+  sink.close();
 
-    return await file.readAsString();
-  }
+  return await file.readAsString();
+}
+
+Future<String> readFile(filename) async {
+  final Directory directory = await getApplicationDocumentsDirectory();
+  final File file = File('${directory.path}/${filename}.txt');
+  return await file.readAsString();
+}
