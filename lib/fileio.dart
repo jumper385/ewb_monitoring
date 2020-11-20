6b1cabd64edba042a1c4ef6fcd4a38cce517ac13
Luuvha <palistha1900@gmail.com>
2020-11-20 11:12:30 +0800

    added path_provider as dependency

--- lib/fileio.dart
+++ lib/fileio.dart
@@ -1,4 +1,5 @@
 library fileio;
+
 import 'package:uuid/uuid.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
@@ -12,7 +13,8 @@ Future<String> writeFile(data_array, data_type, filename, vehicleId) async {
   final File file = await File(new_file).create(recursive: true);
   final data = await file.readAsString();
   final sink = file.openWrite();
-  sink.write(data + "${vehicleId}, ${uuid.v4()}, ${DateTime.now()}, ${data_type}, ${data_array.join(",")}\n\r");
+  sink.write(data +
+      "${vehicleId}, ${uuid.v4()}, ${DateTime.now()}, ${data_type}, ${data_array.join(",")}\n\r");
   sink.close();
 
   return await file.readAsString();
